926a8999c42f5afd49bbbd6ac999a2158ad386bb
Qi Chen <qchen125@gmail.com>
2019-04-04 12:12:22 -0400

    updating script and adding a delete

--- script.sh
+++ script.sh
@@ -1,5 +1,4 @@
-oozie jobs -oozie http://localhost:11000/oozie -kill -filter user=root -jobtype bundle & oozie jobs -oozie http://localhost:11000/oozie -kill -filter user=root
- -jobtype coordinator & oozie jobs -oozie http://localhost:11000/oozie -kill -filter user=root
+oozie jobs -oozie http://localhost:11000/oozie -kill -filter user=root -jobtype bundle & oozie jobs -oozie http://localhost:11000/oozie -kill -filter user=root -jobtype coordinator & oozie jobs -oozie http://localhost:11000/oozie -kill -filter user=root
 
 sqoop job --meta-connect jdbc:hsqldb:hsql://localhost:16000/sqoop --delete cdw_branch_import
 sqoop job --meta-connect jdbc:hsqldb:hsql://localhost:16000/sqoop --delete cdw_credit_import
