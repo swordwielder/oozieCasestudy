e79a8865528933b23bccf06e84d0c3c5a8a3a47c
Qi Chen <qchen125@gmail.com>
2019-04-04 10:17:07 -0400

    added kill oozie jobs
    with --append

--- script.sh
+++ script.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+oozie jobs -oozie http://localhost:11000/oozie -kill -filter user=root -jobtype bundle & oozie jobs -oozie http://localhost:11000/oozie -kill -filter user=root
+-jobtype coordinator & oozie jobs -oozie http://localhost:11000/oozie -kill -filter user=root
 
 sqoop job --meta-connect jdbc:hsqldb:hsql://localhost:16000/sqoop --delete cdw_branch_import
 sqoop job --meta-connect jdbc:hsqldb:hsql://localhost:16000/sqoop --delete cdw_credit_import
