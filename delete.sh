8584715771177e70be19535d637f5adba2f32080
Qi Chen <qchen125@gmail.com>
2019-04-05 09:51:18 -0400

    adding hive drop tables to delete.sh

--- delete.sh
+++ delete.sh
@@ -4,3 +4,13 @@ sqoop job --meta-connect jdbc:hsqldb:hsql://localhost:16000/sqoop --delete cdw_b
 sqoop job --meta-connect jdbc:hsqldb:hsql://localhost:16000/sqoop --delete cdw_credit_import
 sqoop job --meta-connect jdbc:hsqldb:hsql://localhost:16000/sqoop --delete cdw_customer_import
 sqoop job --meta-connect jdbc:hsqldb:hsql://localhost:16000/sqoop --delete cdw_time_import
+
+
+hive -e "Drop Table IF EXISTS cdw_sapp.CDW_Sapp_Branch;"
+hive -e "Drop Table IF EXISTS cdw_sapp.CDW_Sapp_Customer;"
+hive -e "Drop Table IF EXISTS cdw_sapp.CDW_Sapp_Credit;"
+hive -e "Drop Table IF EXISTS cdw_sapp.CDW_Sapp_Time;"
+hive -e "Drop Table IF EXISTS cdw_sapp.BRANCH_PARTITION;"
+hive -e "Drop Table IF EXISTS cdw_sapp.CUSTOMER_PARTITION;"
+hive -e "Drop Table IF EXISTS cdw_sapp.CREDIT_PARTITION;"
+hive -e "Drop Table IF EXISTS cdw_sapp.TIME_PARTITION;"
