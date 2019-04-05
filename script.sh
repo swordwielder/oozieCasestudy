dc3a8eba779702c1b20f5b71a9ef0800ef0a5fe3
Qi Chen <qchen125@gmail.com>
2019-04-05 12:08:20 -0400

    updating script with drop tables

--- script.sh
+++ script.sh
@@ -1,5 +1,15 @@
 oozie jobs -oozie http://localhost:11000/oozie -kill -filter user=root -jobtype bundle & oozie jobs -oozie http://localhost:11000/oozie -kill -filter user=root -jobtype coordinator & oozie jobs -oozie http://localhost:11000/oozie -kill -filter user=root
 
+hive -e "Drop Table IF EXISTS cdw_sapp.CDW_Sapp_Branch;"
+hive -e "Drop Table IF EXISTS cdw_sapp.CDW_Sapp_Customer;"
+hive -e "Drop Table IF EXISTS cdw_sapp.CDW_Sapp_Credit;"
+hive -e "Drop Table IF EXISTS cdw_sapp.CDW_Sapp_Time;"
+hive -e "Drop Table IF EXISTS cdw_sapp.BRANCH_PARTITION;"
+hive -e "Drop Table IF EXISTS cdw_sapp.CUSTOMER_PARTITION;"
+hive -e "Drop Table IF EXISTS cdw_sapp.CREDIT_PARTITION;"
+hive -e "Drop Table IF EXISTS cdw_sapp.TIME_PARTITION;"
+
+
 sqoop job --meta-connect jdbc:hsqldb:hsql://localhost:16000/sqoop --delete cdw_branch_import
 sqoop job --meta-connect jdbc:hsqldb:hsql://localhost:16000/sqoop --delete cdw_credit_import
 sqoop job --meta-connect jdbc:hsqldb:hsql://localhost:16000/sqoop --delete cdw_customer_import
