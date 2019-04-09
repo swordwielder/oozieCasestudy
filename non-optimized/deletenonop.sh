41bc07b02f63734102dbd770b05d9c9c934c07c0
Qi Chen <qchen125@gmail.com>
2019-04-09 09:55:04 -0400

    new files

---
+++ non-optimized/deletenonop.sh
@@ -0,0 +1,16 @@
+oozie jobs -oozie http://localhost:11000/oozie -kill -filter user=root -jobtype bundle & oozie jobs -oozie http://localhost:11000/oozie -kill -filter user=root -jobtype coordinator & oozie jobs -oozie http://localhost:11000/oozie -kill -filter user=root
+
+sqoop job --meta-connect jdbc:hsqldb:hsql://localhost:16000/sqoop --delete nonoptim_branch_import
+sqoop job --meta-connect jdbc:hsqldb:hsql://localhost:16000/sqoop --delete nonoptim_credit_import
+sqoop job --meta-connect jdbc:hsqldb:hsql://localhost:16000/sqoop --delete nonoptim_customer_import
+sqoop job --meta-connect jdbc:hsqldb:hsql://localhost:16000/sqoop --delete nonoptim_time_import
+
+
+hive -e "Drop Table IF EXISTS cdw_sapp.CDW_Sapp_Branch;"
+hive -e "Drop Table IF EXISTS cdw_sapp.CDW_Sapp_Customer;"
+hive -e "Drop Table IF EXISTS cdw_sapp.CDW_Sapp_Credit;"
+hive -e "Drop Table IF EXISTS cdw_sapp.CDW_Sapp_Time;"
+hive -e "Drop Table IF EXISTS cdw_sapp.BRANCH_PARTITION;"
+hive -e "Drop Table IF EXISTS cdw_sapp.CUSTOMER_PARTITION;"
+hive -e "Drop Table IF EXISTS cdw_sapp.CREDIT_PARTITION;"
+hive -e "Drop Table IF EXISTS cdw_sapp.TIME_PARTITION;"
